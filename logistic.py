b4cb9b5bc85f62d8095de6213099017e718ba148
인우 <siw2009@naver.com>
2025-08-07 12:12:34 +0900

    error finding complete

--- logistic.py
+++ logistic.py
@@ -1,4 +1,4 @@
-# from random import random
+# from random import random, randint
 # def generate_x(path: str, start: float, end: float, cnt: int):
 #     rlt = [random() * (end - start) + start for _ in range(cnt)]
 #     with open(path, 'w') as f:  f.write('')
@@ -8,6 +8,13 @@
 
 # for path in ['./inputs/x1-input.txt', './inputs/x2-input.txt', './inputs/w1-input.txt', './inputs/w2-input.txt', './inputs/b-input.txt', ]:
 #     generate_x(path, -3, 3, 10000)
+
+# with open('./inputs/real.txt', 'w') as f:  f.write('')
+# with open('./inputs/real.txt', 'a') as f:
+#     for i in range(10000):
+#         f.write(str(randint(0,1))+'\n')
+
+# exit()
 
 
 
@@ -21,6 +28,10 @@ def sigmoid(x: float) -> float:
 
 def discrete(x: float) -> int:
     return 1  if x>0.5 else  0
+
+
+def predict(input_data: list[list[float]]) -> list[float]:
+    return [sigmoid(input_data[0][i]*input_data[2][i] + input_data[1][i]*input_data[3][i] + input_data[4][i]) for i in range(10000)]
 
 
 def err(realdata: float, prediction: float) -> float:
@@ -39,5 +50,8 @@ def read_data(paths: list[str]) -> list[list[float]]:
     return rlt
 
 
+
 input_data = read_data(['./inputs/x1-input.txt', './inputs/x2-input.txt', './inputs/w1-input.txt', './inputs/w2-input.txt', './inputs/b-input.txt'])
-print([discrete(sigmoid(input_data[0][i]*input_data[2][i] + input_data[1][i]*input_data[3][i] + input_data[4][i])) for i in range(10000)])
+real_data = read_data(['./inputs/real.txt'])[0]
+prediction = predict(input_data)
+print([err(real_data[i], prediction[i]) for i in range(10000)])
