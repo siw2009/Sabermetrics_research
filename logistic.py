93f8d2c1bdd407f83a9439a0634c5ad0d995ac25
인우 <siw2009@naver.com>
2025-08-07 10:44:02 +0900

    a

--- logistic.py
+++ logistic.py
@@ -1,16 +1,24 @@
-from random import random
-def generate_x(path: str, start: float, end: float, cnt: int):
-    rlt = [random() * (end - start) + start for _ in range(cnt)]
-    with open(path, 'w') as f:  f.write('')
-    with open(path, 'a') as f:
-        for x in rlt:
-            f.write(str(x) + '\n')
+# from random import random
+# def generate_x(path: str, start: float, end: float, cnt: int):
+#     rlt = [random() * (end - start) + start for _ in range(cnt)]
+#     with open(path, 'w') as f:  f.write('')
+#     with open(path, 'a') as f:
+#         for x in rlt:
+#             f.write(str(x) + '\n')
 
+# for path in ['./inputs/x1-input.txt', './inputs/x2-input.txt', './inputs/w1-input.txt', './inputs/w2-input.txt', './inputs/b-input.txt', ]:
+#     generate_x(path, -100, 100, 10000)
+
+from math import e
+def sigmoid(x: float) -> float:
+    return 1 / 1+e**(-x)
+
+input_data = []
 for path in ['./inputs/x1-input.txt', './inputs/x2-input.txt', './inputs/w1-input.txt', './inputs/w2-input.txt', './inputs/b-input.txt', ]:
-    generate_x(path, -100, 100, 10000)
+    input_data.append([])
+    with open(path, 'r') as f:
+        for i in range(10000):
+            input_data[-1].append(float(f.readline()))
 
-# from math import e
-# def sigmoid(x: float) -> float:
-#     return 1 / 1+e**(-x)
-
+print(input_data[0])
 # print([sigmoid() for i in range(10000)])
