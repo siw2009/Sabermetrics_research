f5b3f99cdafccabb9fa23567b4f8ac5ddd9dffb2
인우 <siw2009@naver.com>
2025-08-07 10:31:37 +0900

    wow

--- logistic.py
+++ logistic.py
@@ -1,12 +1,16 @@
-# from random import random
-# def generate_x(path: str, start: float, end: float, cnt: int):
-#     rlt = [random() * (end - start) + start for _ in range(cnt)]
-#     with open(path, 'w') as f:  f.write('')
-#     with open(path, 'a') as f:
-#         for x in rlt:
-#             f.write(str(x) + '\n')
+from random import random
+def generate_x(path: str, start: float, end: float, cnt: int):
+    rlt = [random() * (end - start) + start for _ in range(cnt)]
+    with open(path, 'w') as f:  f.write('')
+    with open(path, 'a') as f:
+        for x in rlt:
+            f.write(str(x) + '\n')
 
-# # generate_x('./inputs/x1-input.txt', -100, 100, 10000)
-# # generate_x('./inputs/x2-input.txt', 0, 1000, 10000)
-# # generate_x('./inputs/b-input.txt', -10, 10, 10000)
+for path in ['./inputs/x1-input.txt', './inputs/x2-input.txt', './inputs/w1-input.txt', './inputs/w2-input.txt', './inputs/b-input.txt', ]:
+    generate_x(path, -100, 100, 10000)
 
+# from math import e
+# def sigmoid(x: float) -> float:
+#     return 1 / 1+e**(-x)
+
+# print([sigmoid() for i in range(10000)])
