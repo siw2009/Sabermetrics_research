fae19f823ae597ab513fd158fad7fd94a41dfe2c
인우 <siw2009@naver.com>
2025-08-07 10:20:41 +0900

    주석화

--- logistic.py
+++ logistic.py
@@ -1,11 +1,12 @@
-from random import random
-def generate_x(path: str, start: float, end: float, cnt: int):
-    rlt = [random() * (end - start) + start for _ in range(cnt)]
-    with open(path, 'w') as f:  f.write('')
-    with open(path, 'a') as f:
-        for x in rlt:
-            f.write(str(x) + '\n')
+# from random import random
+# def generate_x(path: str, start: float, end: float, cnt: int):
+#     rlt = [random() * (end - start) + start for _ in range(cnt)]
+#     with open(path, 'w') as f:  f.write('')
+#     with open(path, 'a') as f:
+#         for x in rlt:
+#             f.write(str(x) + '\n')
 
-# generate_x('./inputs/x1-input.txt', -100, 100, 10000)
-# generate_x('./inputs/x2-input.txt', 0, 1000, 10000)
-# generate_x('./inputs/b-input.txt', -10, 10, 10000)
+# # generate_x('./inputs/x1-input.txt', -100, 100, 10000)
+# # generate_x('./inputs/x2-input.txt', 0, 1000, 10000)
+# # generate_x('./inputs/b-input.txt', -10, 10, 10000)
+
