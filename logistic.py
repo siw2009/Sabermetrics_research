90c514a63012b4eabc0fdd606ba213ff592e9a8e
인우 <siw2009@naver.com>
2025-08-07 11:55:31 +0900

    functions add

--- logistic.py
+++ logistic.py
@@ -7,18 +7,21 @@
 #             f.write(str(x) + '\n')
 
 # for path in ['./inputs/x1-input.txt', './inputs/x2-input.txt', './inputs/w1-input.txt', './inputs/w2-input.txt', './inputs/b-input.txt', ]:
-#     generate_x(path, -100, 100, 10000)
+#     generate_x(path, -3, 3, 10000)
 
 from math import e
 def sigmoid(x: float) -> float:
-    return 1 / 1+e**(-x)
+    return 1 / (1+e**(-x))
+
+def discrete(x: float) -> int:
+    return 1  if x>0.5 else  0
+
 
 input_data = []
-for path in ['./inputs/x1-input.txt', './inputs/x2-input.txt', './inputs/w1-input.txt', './inputs/w2-input.txt', './inputs/b-input.txt', ]:
+for path in ['./inputs/x1-input.txt', './inputs/x2-input.txt', './inputs/w1-input.txt', './inputs/w2-input.txt', './inputs/b-input.txt']:
     input_data.append([])
     with open(path, 'r') as f:
         for i in range(10000):
             input_data[-1].append(float(f.readline()))
 
-print(input_data[0])
-# print([sigmoid() for i in range(10000)])
+print([discrete(sigmoid(input_data[0][i]*input_data[2][i] + input_data[1][i]*input_data[3][i] + input_data[4][i])) for i in range(10000)])
