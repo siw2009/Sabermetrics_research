37add4a862957292f41751863a3161cd1246d815
인우 <siw2009@naver.com>
2025-08-10 23:01:56 +0900

    progress feels like it needs some fixes

--- data_reader.py
+++ data_reader.py
@@ -18,3 +18,18 @@ def read_csv(path: str, types: list) -> list[list]:
             rlt.append(apply_types(file.readline().strip().split(','), types))
 
     return rlt
+
+
+def split_data(data: list[list], split_by: tuple) -> list[list[list]]:
+    if sum(split_by) > len(data[0]):  return []
+    for i in split_by:
+        if i<0:  return []
+
+    rlt = [[] for _ in range(len(split_by))]
+    for i in range(len(data)):
+        idx = 0
+        for j in range(len(split_by)):
+            rlt[j].append(data[i][idx:idx + split_by[j]])
+            idx += split_by[j]
+    
+    return rlt
--- logistic.py
+++ logistic.py
@@ -1,5 +1,6 @@
-from math import e, log1p
+from math import e, log
 from random import random, randint
+from data_reader import *
 
 
 
@@ -12,6 +13,7 @@ def discrete(x: float) -> int:
 
 
 def predict(input_data: list[float], weights: list[float], bias: float) -> float:
+    # print(sum([input_data[i] * weights[i] for i in range(len(weights))]) + bias)
     return sigmoid(sum([input_data[i] * weights[i] for i in range(len(weights))]) + bias)
 
 
@@ -20,11 +22,23 @@ def slope(realdata: int, prediction: float, inputval: list[float] = [1.0]) -> li
 
 
 def err(realdata: int, prediction: float) -> float:
-    return (realdata-1) * log1p(-prediction) - realdata * log1p(prediction-1)
+    if prediction == 1:  return 0
+    return (realdata-1) * log(-prediction+1, e) - realdata * log(prediction, e)
 
 
-def learn(weights: list[float], slopes: list[float], step: float) -> list[float]:
+def learn_row(weights: list[float], slopes: list[float], step: float) -> list[float]:
     return [weights[i] + step * slopes[i]  for i in range(len(weights))]
+
+
+def learn(data: list[list], real_data: list[list], weight: list[float], bias: float, step: float) -> tuple[list[float], float]:
+    weight_rlt = weight.copy()
+    bias_rlt = bias
+    for i in range(len(data)):
+        prediction = predict(data[i], weight_rlt, bias_rlt)
+        weight_rlt = learn_row(weight_rlt, slope(real_data[i][0], prediction, data[i]), step)
+        bias_rlt = learn_row([bias_rlt], slope(real_data[i][0], prediction), step)[0]
+    
+    return weight_rlt, bias_rlt
 
 
 def read_data(paths: list[str]) -> list[list[float]]:
@@ -40,17 +54,27 @@ def read_data(paths: list[str]) -> list[list[float]]:
 
 
 
-step = 0.5
-inputval = [random() * 6 -3  for _ in range(2)]
-real_data = randint(0,1)
-weight = [random() * 10 - 5  for _ in range(2)]
-bias = random() * 5 - 2
+data = read_csv('./inputs/darwin (testdata).csv', [int, str, str] + [float] * 19 + [int, int])[1:]
+data_split = split_data(data, (3, 20, 1))
 
 
-for i in range(10 ** 8):
-    prediction = predict(inputval, weight, bias)
-    weight = learn(weight, slope(real_data, prediction, inputval), step)
-    bias = learn([bias], slope(real_data, prediction), step)[0]
+step = 0.0001
+# inputval = [random() * 6 -3  for _ in range(2)]
+inputval = data_split[1][:-1]
+# real_data = randint(0,1)
+real_data = data_split[2][:-1]
+# weight = [random() * 10 - 5  for _ in range(len(data_split[1][0]))]
+weight = [0] * (len(data_split[1][0]))
+# bias = random() * 5 - 2
+bias = random()
 
-    if i%10**6 == 0:
-        print(err(real_data, prediction))
+
+err_pred = predict(data_split[1][-1], weight, bias)
+print(err(data_split[2][-1][0], err_pred))
+for i in range(10):
+    weight, bias = learn(inputval, real_data, weight, bias, step)
+
+    # if i%10**6 == 0:
+    #     print(err(real_data, prediction))
+print(weight)
+print(bias)
