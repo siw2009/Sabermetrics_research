5d73b0b4458c7d6b9365f9b834cd8d847bbd7664
astatinegithub <astatine.147@gmail.com>
2025-08-13 18:17:59 +0900

    aaaa

--- test_sources/KBO_scrapping_code.py
+++ test_sources/KBO_scrapping_code.py
@@ -2,7 +2,6 @@ import time
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from io import StringIO
 import time
--- test_sources/sklearn_learn_learn.py
+++ test_sources/sklearn_learn_learn.py
@@ -1,4 +1,13 @@
-import sklearn
-import seaborn
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
 
-sklearn.linear_model.
+# df = pd.read_csv('./inputs/weatherAUS (testdata).csv')
+
+X = np.array(1,10)
+Y = X*5
+
+plt.figure((12,8), facecolor="#000000", edgecolor="#000000", frameon=False)
+
+plt.plot([X,Y,'r'])
+plt.show()
